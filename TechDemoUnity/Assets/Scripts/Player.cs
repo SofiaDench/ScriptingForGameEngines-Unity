0bfa7d7cad69427c15b7bc900d2faed4118e8a77
Sofia DENCH <123573016+SofiaDench@users.noreply.github.com>
2023-04-25 13:14:05 +0100

    MOVEMENT ACHIEVED!
    Player script is  attached to the player, at present players attack is it hatchng a bomb

--- TechDemoUnity/Assets/Scripts/Player.cs
+++ TechDemoUnity/Assets/Scripts/Player.cs
@@ -4,15 +4,98 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Variables
 
-    // Update is called once per frame
+    public float movementSpeed;
+    public GameObject camera;
+
+    public Transform playerObject;
+
+    public GameObject bulletSpawnPoint;
+    public GameObject bullet;
+    public float waitTime;
+
+    private Transform bulletSpawned;
+
+    public float points;
+
+    public float maxHealth;
+    public float health;
+
+
+    //Methods
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
     void Update()
     {
-        
+        //Player facing mouse
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        float histDist = 0.0f;
+
+        if (playerPlane.Raycast(ray, out histDist))
+        {
+            Vector3 targetPoint = ray.GetPoint(histDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+
+            playerObject.transform.rotation = Quaternion.Slerp(playerObject.transform.rotation, targetRotation, 7f * Time.deltaTime);
+        }
+
+
+        //Player Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+        }
+
+        //Shooting
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+        //Player Death
+
+        if (health <= 0)
+        {
+            Die();
+        }
+
+
+    }//end of void Update
+
+    void Shoot()
+    {
+        bulletSpawned = Instantiate(bullet.transform, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
+        bulletSpawned.rotation = bulletSpawnPoint.transform.rotation;
     }
+    public void Die()
+    {
+        print("You are dead");
+    }
+
+
+
 }
+
+
+
+
+
